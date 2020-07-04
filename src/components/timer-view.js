0440f0489faa52332fe743b6c4654a5c5c42b3cf
aucoeur <aucoeur.ngo@students.makeschool.com>
2020-07-04 16:03:07 -0700

    styled timer view

--- src/components/timer-view.js
+++ src/components/timer-view.js
@@ -4,6 +4,8 @@ import { connect } from 'react-redux';
 import { toggleTimer } from '../actions';
 import { formatTime } from '../utils';
 
+import './timer-view.css'
+
 class TimerView extends Component {
     constructor(props) {
         super(props)
@@ -11,12 +13,13 @@ class TimerView extends Component {
 
     // Timer should have its name, time and a start/stop button (logic for this button will be built out later)
     render() {
-        const { index, toggleTimer, timer } = this.props
+        const { index, toggleTimer, timer } = this.props;
+        const buttonClass = timer.isRunning ? "stop" : "start";
         return (
-            <div>
-                <h2>{timer.name}</h2>
-                    <h1>{formatTime(timer.time)}</h1>
-                <button 
+            <div className='timer-view'>
+                <h2 className='timer-view-name__h2'>{timer.name}</h2>
+                    <h1 className='timer-view-time__h1'>{formatTime(timer.time)}</h1>
+                <button className={`timer-view__button timer-view__button--${buttonClass}`}
                     // This calls our toggleTimer action on the specific timer (specified by the index)
                     onClick={ (e) => {
                         toggleTimer(index)
